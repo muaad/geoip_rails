a2792de70056aca92bc570fc76516695b3fe7f6e
Muaad Abdirahman <muadh24@gmail.com>
2014-06-20 17:57:46 +0300

    added support for xml and csv formats to go along with json

--- lib/geoip_rails.rb
+++ lib/geoip_rails.rb
@@ -3,9 +3,21 @@ require "httparty"
 
 module GeoipRails
   module_function
+
   def find_by_ip ip
-  	format = "json"
-  	HTTParty.get("http://freegeoip.net/#{format}/#{ip}")
+    HTTParty.get("http://freegeoip.net/json/#{ip}")
+  end
+  
+  def find_by_ip_and_format ip, format
+    `wget "http://freegeoip.net/#{format}/#{ip}"`
+    response = ""
+    File.open("#{ip}").each { |line|  response += line}
+    return response
+    `rm -rf #{ip}`
+  end
+
+  def geolocate ip
+  	HTTParty.get("http://freegeoip.net/json/#{ip}")
   end
 
   # def geolocation
@@ -16,30 +28,39 @@ module GeoipRails
   def country ip
   	find_by_ip(ip)["country_name"]
   end
+
   def country_code ip
   	find_by_ip(ip)["country_code"]
   end
+
   def city ip
   	find_by_ip(ip)["city"]
   end
+
   def region_code ip
   	find_by_ip(ip)["region_code"]
   end
+
   def region_name ip
   	find_by_ip(ip)["region_name"]
   end
+
   def zipcode ip
   	find_by_ip(ip)["zipcode"]
   end
+
   def latitude ip
   	find_by_ip(ip)["latitude"]
   end
+
   def longitude ip
   	find_by_ip(ip)["longitude"]
   end
+
   def metro_code ip
   	find_by_ip(ip)["metro_code"]
   end
+
   def area_code ip
   	find_by_ip(ip)["area_code"]
   end
